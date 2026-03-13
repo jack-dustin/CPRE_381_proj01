416fb58564682e87d7ebabdf9535fc0e49afa67b
ipridie <ipridie@co2050-22.ece.iastate.edu>
2026-03-13 11:51:04 -0500

    Actually I think the ALU was already in the branch. lol. Anyways... I am adding some commented out broken files. Good Luck Have Fun

--- proj01_ALU/xor_N.vhd
+++ proj01_ALU/xor_N.vhd
@@ -3,24 +3,29 @@
     -- Apparently I can't just generate 32 instancs of xor gates and hook a vector up to it. 
 -- 2.17.2026,   9:30 MM
 
+
+
+
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 -- N-Bit xor
+-- Natural ~= Unsigned* Integer*
 entity xor_N is
-    generic(N : natural := 32); -- Natural ~= Unsigned* Integer*
-    port(i_A : in std_logic_vector(N-1 downto 0);
-         i_B : in std_logic_vector(N-1 downto 0);
-         o_F : out std_logic_vector(N-1 downto 0));
-end entity;
+    generic(N : natural := 32); 
+    port(i_A : in  std_logic_vector(N-1 downto 0);
+         i_B : in  std_logic_vector(N-1 downto 0);
+         o_F : out std_logic_vector(N-1 downto 0) );
+end xor_N;
 
 architecture structural of xor_N is
 
     component xorg2 is
         port(i_A          : in std_logic;
              i_B          : in std_logic;
-             o_F          : out std_logic);
+             o_F          : out std_logic );
     end component;
 
 begin
